ffda7de2f35a590e48188e1319ebb939a70fbf04
Mario <mlivia@dc.uba.ar>
2026-05-22 19:19:38 -0300

    Cambio logo por uno adecuado y agrego estilos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import logo from "../../assets/react.svg";
 import { Nav } from "../Nav/Nav.jsx";
+import './Header.css';
 
 export const Header = () => {
+    const pathLogo = "/img/ittf_logo.png";
     return (
-        <header>
-            <img src={logo} alt="Logo de React" />
+        <header className="header">
+            <img src={pathLogo} alt="Logo de ITTF" className="img-logo" />
             <Nav />
         </header>
     )
