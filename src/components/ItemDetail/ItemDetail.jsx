18a4ce9095348379eeab40b44bc0cbb3101a0153
Mario <mlivia@dc.uba.ar>
2026-05-26 13:41:47 -0300

    agrego evento al boton "agregar carrito" para hacer lo que debe

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,9 +1,12 @@
+import { useCart } from "../../context/CartContext.jsx";
 import { Item } from "../Item/Item.jsx";
 
 export const ItemDetail = ({ item }) => {
+    const {addToCart} = useCart();
+
     return (
         <Item {...item}>
-            <button className="btn primary">Agregar al carrito</button>
+            <button onClick = {() => addToCart(item)} className="btn primary">Agregar al carrito</button>
         </Item>
     )
 }
