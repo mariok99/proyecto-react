035778d556b92459ce27dbd785becf0ec31e74be
Mario <mlivia@dc.uba.ar>
2026-05-26 13:42:21 -0300

    elimino console log y agrego notas

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,6 +1,5 @@
 import "./Item.css"
 export const Item = ({ title, description, price, image, children }) => {
-    console.log({ title, description, price, image })
     return (
         <article className="card">
             <h3>{title}</h3>
