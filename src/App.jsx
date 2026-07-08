679587a51345e935e2fa090df447ff977ba7f394
Mario <mlivia@dc.uba.ar>
2026-07-08 15:33:35 -0300

    Eliminación de codigo viejo y reorganización de rutas para implementar protección de rutas y login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import { Header } from './components/Header/Header.jsx'
 import { Footer } from './components/Footer/Footer.jsx'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer.jsx'
 import { Routes, Route } from "react-router-dom"
@@ -9,13 +7,17 @@ import { FormContainer } from './Form/FormContainer.jsx'
 import { CartView } from './components/Cart/CartView.jsx'
 import { ProductFormContainer } from './components/adminComponents/ProductFormContainer.jsx'
 import { ProductSuccess } from './components/adminComponents/ProductFormSuccess.jsx'
+import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute.jsx'
+import { AdminLayout } from './layouts/AdminLayout.jsx'
+import { PublicLayout } from './layouts/PublicLayout.jsx'
+import { Dashboard } from './components/adminComponents/Dashboard/Dashboard.jsx'
+import { Navigate } from 'react-router-dom'
+import { Login } from './components/Login/Login.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
       <main>
         {/* Dentro de Routes van las rutas que va a tener la app
         No puede poner nada mas, por ejemplo, si pongo un componente
@@ -23,18 +25,32 @@ function App() {
         <Routes>
           {/*cada ruta tiene element, que es el componente
           que va a renderizarse luego de elegir ir a esa ruta */}
-          <Route path="/" element={<ItemListContainer />} />
-          <Route path="/category/:category" element={<ItemListContainer />} />
-          <Route path="/cart" element={<CartView />} />
-          <Route path="/products" element={<ItemListContainer />} />
-          <Route path="/product/:id" element={<ItemDetailContainer />} />
-          <Route path="/form" element={<FormContainer />} />
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/category/:category" element={<ItemListContainer />} />
+            <Route path="/cart" element={<CartView />} />
+            <Route path="/products" element={<ItemListContainer />} />
+            <Route path="/product/:id" element={<ItemDetailContainer />} />
+            <Route path="/form" element={<FormContainer />} />
+          </Route>
 
-          <Route path="/admin" element={<ProductFormContainer />} />
-          <Route path="/success/:id" element={<ProductSuccess />} />
+          <Route path="/admin/login" element={<Login />} />
+
+          {/* Anidación de routes, para rutas protegidas */}
+          <Route path="/admin" element={
+            <ProtectedRoute>
+              <AdminLayout />
+            </ProtectedRoute>
+          } >
+            {/* si entra directamente a "/admin" lo mandamos a "/admin/dashboard" */}
+            <Route index element={<Navigate to={"dashboard"} />} />
+            <Route path="dashboard" element={<Dashboard />} />
+
+            <Route path="products/new" element={<ProductFormContainer />} />
+            <Route path="products/success/:id" element={<ProductSuccess />} />
+          </Route>
         </Routes>
       </main>
-      <Footer />
     </>
   )
 }
