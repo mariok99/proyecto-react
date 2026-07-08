e0ed0269a909d626fec88e72e5430e4b022685d1
Mario <mlivia@dc.uba.ar>
2026-07-08 15:34:26 -0300

    Agrego authContext para login

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useContext } from "react";
+import { useEffectLoading } from "../utils/myUseEffect.jsx";
+import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../firebase/config.js";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+}
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const callback = () => {
+        const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+            setUser(firebaseUser);
+        })
+        return unsubscribe;
+    }
+
+    useEffectLoading(callback, [], setLoading);
+
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.log("Occur an error while logging out", error);
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
